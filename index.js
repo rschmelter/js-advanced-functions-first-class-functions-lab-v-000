3ed6d85593a8f3031fb40bdefbb172d18ea17ee4
Ryan Schmelter <rschmelter33@gmail.com>
2018-08-12 20:02:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,4 +9,6 @@ const returnLastTwoDrivers = function (drivers) {
   return drivers.slice(Math.max(drivers.length -2, 1))
 }
 
-selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
